360716a56716f891a72e818c5531f96544ae3237
Riza Fahmi <rizafahmi@gmail.com>
2017-06-11 19:28:32 +0700

    Mocking up the template

--- Todo.elm
+++ Todo.elm
@@ -1,7 +1,23 @@
 module Todo exposing (..)
 
-import Html exposing (h1, text)
+import Html exposing (h1, text, div, form, input, ul, li)
+import Html.Attributes exposing (class, type_, placeholder)
+
+
+view =
+    div [ class "container" ]
+        [ h1 [] [ text "Elm Todo" ]
+        , form []
+            [ input [ type_ "text", placeholder "What you want to do?" ] []
+            , input [ type_ "submit" ] [ text "Do!" ]
+            ]
+        , ul []
+            [ li [] [ text "todo" ]
+            , li [] [ text "todo" ]
+            , li [] [ text "todo" ]
+            ]
+        ]
 
 
 main =
-    h1 [] [ text "Elm Todo" ]
+    view
